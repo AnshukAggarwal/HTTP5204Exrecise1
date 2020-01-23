7d536a531f630efa4399d86102ea1f3cdc04d799
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2020-01-22 21:32:53 -0500

    first commit

---
+++ PetGroomingMVC/PetGrooming/Models/GroomBooking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetGrooming.Models
+{
+    public class GroomBooking
+    {
+        /*
+            A GroomBooking is an agreement between an owner and a groomer to provide services for a pet
+            
+            Some things that describe a GroomBooking
+                - A date and time
+                - Price
+                */
+            //Now create the base piece of info for this class.
+            [Key]
+            public int GroomBookingID { get; set; }
+            public DateTime GroomBookingTime { get; set; }
+            public double GroomBookingPrice { get; set; }
+
+          /*  A GroomBooking must reference
+                - A Groomer
+                - A Pet
+                - An Owner
+                - A list of GroomServices
+                
+        */
+    }
+}
---
+++ PetGroomingMVC/PetGrooming/Models/Groomer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetGrooming.Models
+{
+    public class Groomer
+    {
+        /* 
+            A groomer is someone who is employed to groom pets
+            Some things that describe a groomer
+                - First Name
+                - Last Name
+                - Date of Birth
+                - Phone Number
+                - Hourly Rate
+                */
+                //Base Info for this class
+                [Key]
+                public int GroomerID { get; set; }
+                public string GroomerFname { get; set; }
+                public string GroomerLname { get; set; }
+                public string GroomerPhone { get; set; }
+                public double GroomerRate { get; set; }
+                public int GroomerExperiene { get; set; }
+
+           /* A booking must reference to a groomer
+        */
+    }
+}
